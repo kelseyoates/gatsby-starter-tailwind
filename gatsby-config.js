fcbf6ce80e7122ccc109ce6aff920aea8638a83a
Kelsey Oates <kelsey.oates@gmail.com>
2018-08-23 16:30:54 -0400

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Tailwind',
+    title: 'Kelsey Oates | UX Designer and Writer',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
