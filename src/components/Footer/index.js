20065dc73bc7680220e5eef561ada4fc6e933b95
Kelsey Oates <kelsey.oates@gmail.com>
2018-08-23 16:19:15 -0400

    Add files via upload

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,29 @@
 import React from "react";
 
 const Footer = () => (
-  <div className="bg-blue">
-    <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
-      <p className="text-white">
-        Created by{" "}
+  <div className="bg-white mx-auto mt-5">
+    <div className="flex justify-between max-w-xl mx-auto p-2">
+      <p className="footer">
+      <a
+          href="https://www.linkedin.com/in/kelsey-oates"
+          className="no-underline"
+        >
+        	&copy; 2018 Kelsey Oates
+        </a> based on <a
+          href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+          className="no-underline"
+        >
+         Gatsby Starter Tailwind
+        </a> by{" "}
         <a
           href="https://taylorbryant.blog"
-          className="font-bold no-underline text-white"
+          className="no-underline"
         >
           Taylor Bryant
         </a>
       </p>
       <p>
-        <a
-          href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-          className="font-bold no-underline text-white"
-        >
-          GitHub
-        </a>
+        
       </p>
     </div>
   </div>
