8dedb32c4df178d8848b6360cb3d2c809e523676
Kelsey Oates <kelsey.oates@gmail.com>
2018-08-26 10:07:24 -0400

    Add files via upload

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -4,12 +4,12 @@ import oprah from "../images/oprah.gif";
 const ThanksPage = () => (
 
 <div className=" flex flex-wrap">
-  <div className="sm:w-1/1 md:w-1/2 lg:w-1/2">
+  <div className="sm:w-1/1 md:w-1/2 px-4 text-center">
     <img src={oprah} />
     </div>
-    <div className="sm:w-1/1 md:w-1/2 lg:w-1/2">
+    <div className="sm:w-1/1 md:w-1/2 px-4">
     <h2>
-      And you get a confirmation message, and you get a confirmation message, and you get a confirmation message...
+      And you get a confirmation message! And you get a confirmation message! And you get a confirmation message!
     </h2>
     <h3>But seriously, your message has gone through and I will get back to you as soon as possible. Thanks!</h3>
     </div>
