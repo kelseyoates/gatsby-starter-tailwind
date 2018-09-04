b8d7ebf027403ce575fd27eec77de228e10dba86
Kelsey Oates <kelsey.oates@gmail.com>
2018-09-04 14:41:04 -0400

    Add files via upload

---
+++ src/pages/the-deadline.js
@@ -0,0 +1,230 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const DeadlinePage = () => (
+  <div>
+
+
+<div className="flex flex-wrap bg-6 items-center text-center">
+
+  <div className="sm:w-1/1 md:w-1/1">
+    <h1 className="text-white">
+     The Deadline
+    </h1>
+    <p className="text-white">
+    Read online for free or purchase to download.
+    </p>
+
+</div>
+
+
+</div>
+
+
+        <div className="flex flex-col md:flex-row px-1 pt-5"> 
+            <section className="w-1/3 md:w-1/3 px-1 pt-5">
+                <h3 className="small">Description</h3>
+        <p>Dorothy Dot must fight off a rival newspaperwoman to get the scoop of the day and save her career.</p>
+
+          <h3>Purchase to Download</h3>
+        <p>Buy the play and you'll receive a PDF of the script.</p>
+
+           <a href="https://gum.co/wHARM" target="blank" className="no-underline "><button className="border-grey-darker hover:border-grey-dark bg-dark-purple hover:bg-pink font-bold px-6 py-3 rounded text-sm text-white">Buy Play</button></a>
+
+    </section>
+
+   <section className="md:w-2/3 px-1 pt-5 font-serif">
+   <h3 className="text-center">
+     The Deadline
+    </h3>
+
+<p className="setting">
+place: New York City<br />
+    time: 1940s </p>
+
+
+<p className="character-list">
+Characters:<br />
+Dorothy Dot <br />
+Shirley McWhirl <br />
+Fred <br />
+Harry Happening <br />
+Voice <br />
+Rusty </p>
+
+<p className="info">This play is inspired by classic films about newspaper people. </p>
+<br />
+<p className="stage">Dorothy Dot approaches Harry Happening at a table.</p>
+<p className="character">Dorothy</p>
+<p> Dorothy Dot. Are you my source?</p>
+<p className="character">Harry</p>
+<p> A lady reporter. All right, I’ll go with it.</p>
+<p className="character">Dorothy</p>
+<p> I need your information right now, I’m on deadline. You said on the phone that the Mayor is doing terrible things to conserve power. What are they?</p>
+<p className="character">Harry</p>
+<p> And you promise I’ll be protected?</p>
+<p className="character">Dorothy</p>
+<p> Of course.</p>
+<p className="character">Harry</p>
+<p> Okay. The mayor...has set up an illegal club.</p>
+<p className="character">Dorothy</p>
+<p> What?</p>
+<p className="character">Harry</p>
+<p> He’s set up a speakeasy, with dancing girls, fringe, and bathtub gin. He makes it himself. And drinks it down every single night.</p>
+<p className="character">Dorothy</p>
+<p> Is this the truth?</p>
+<p className="character">Harry</p>
+<p> Absolutely.</p>
+<p className="character">Dorothy</p>
+<p> It’ll go to print tonight. Thank you.</p>
+<p className="character">Harry</p>
+<p> Of course. And what do I get for helping you, and on such short notice?</p>
+<p className="character">Dorothy</p>
+<p> A front page story that takes down your enemy and a hearty thank you from the Midtown Times.</p>
+<p className="stage">Blackout. Lights up. They’re in the office. Dorothy hands the story to Fred.</p>
+<p className="character">Dorothy</p>
+<p> I got it, Fred.</p>
+<p className="character">Fred</p>
+<p> Fantastic. I’ll put it in right after lunch. Is it good?</p>
+<p className="character">Dorothy</p>
+<p> One of my best yet.</p>
+<p className="stage">Suddenly, Shirley McWhirl appears.</p>
+<p className="character">Shirley</p>
+<p> Really.</p>
+<p className="character">Dorothy</p>
+<p> Why if it isn’t Shirley McWhirl, my esteemed colleague. How are the Irish funnies?</p>
+<p className="character">Shirley</p>
+<p> A lot of important people die.</p>
+<p className="character">Dorothy</p>
+<p> No one who’s important enough to get bumped up above page 12.</p>
+<p className="character">Shirley</p>
+<p> I think it’s funny that you’re so happy about your new story.</p>
+<p className="character">Dorothy</p>
+<p> It’s a great story. See, I actually had to do something to get it, because, you know, I’m a real reporter.</p>
+<p className="character">Shirley</p>
+<p> Are you? Because I’m pretty sure a real reporter would know when her source is lying to her.</p>
+<p className="character">Dorothy</p>
+<p> What are you talking about?</p>
+<p className="character">Shirley</p>
+<p> Isn’t the man you were supposed to meet a redhead?</p>
+<p className="character">Dorothy</p>
+<p> No, I don’t think-</p>
+<p className="character">Shirley</p>
+<p> And isn’t he supposed to be quite tall? With a very deep voice?</p>
+<p className="character">Dorothy</p>
+<p> My source was a short brown-headed man. What are you talking about?</p>
+<p className="character">Shirley</p>
+<p> Hmm. It sounds to me like your source wasn’t even your source. Like it was a fake, an impostor, someone pretending to be your source while another reporter got the real scoop.</p>
+<p className="character">Dorothy</p>
+<p> You didn’t.</p>
+<p className="character">Shirley</p>
+<p> What, set up a fake meeting with a fake man so you would file a bad story and embarrass yourself and get fired? Of course I did.</p>
+<p className="character">Dorothy</p>
+<p> Where is he?</p>
+<p className="character">Shirley</p>
+<p> He’s off somewhere, waiting for me. I’m gonna get that scoop if it’s the last thing I do.</p>
+<p className="character">Dorothy</p>
+<p> If you get that scoop it will be the last thing you do.</p>
+<p className="stage">Fred reenters.</p>
+<p className="character">Fred</p>
+<p> Hey, guys, I’m gonna take an hour for lunch. I’ll put the story in after that.</p>
+<p>Shirley pushes down Dorothy and runs away.</p>
+<p className="character">Shirley</p>
+<p> You’ll never find him!</p>
+<p className="stage">Dorothy runs to her desk and finds information. She picks up the telephone and dials.</p>
+<p className="character">Dorothy</p>
+<p> Hello? Is this the wife of the man I was supposed to speak with today? I don’t know his name, just this number.</p>
+<p>VOICE You mean Rusty? Yes, he told me he had an important meeting today. Something about the Mayor.</p>
+<p className="character">Dorothy</p>
+<p> Yes! That’s him. Where is he now?</p>
+<p>VOICE On his way to a meeting.</p>
+<p className="character">Dorothy</p>
+<p> Where is he going?</p>
+<p>VOICE I’m not sure.</p>
+<p className="character">Dorothy</p>
+<p> Can you think of anything? I need to see him immediately.</p>
+<p>VOICE Let me look around, I might have it written down somewhere.</p>
+<p className="stage">Dorothy is close to panicking, looking around and motioning for her to hurry up.</p>
+<p className="character">Voice (Cont'd)</p><p> Found it. He’s at the 4th Street diner, 11 am.</p>
+<p className="stage">Dorothy runs out without hanging up the phone.</p>
+<p className="character">Voice (Cont'd)</p><p> Hello? Hello?</p>
+<p className="stage">In a scene change, Dorothy rushes into the restaurant and sees Rusty sitting at a table with a cup of coffee. She runs up to him but is bumped by Shirley.</p>
+<p className="character">Rusty</p>
+<p> Dorothy?</p>
+<p className="character">Dorothy and Shirley</p>
+<p>  That’s me.</p>
+<p className="character">Rusty</p>
+<p> You’re both Dorothy?</p>
+<p className="character">Dorothy</p>
+<p> No, just me.</p>
+<p className="character">Shirley</p>
+<p> Stop lying, I’m Dorothy.</p>
+<p className="character">Dorothy</p>
+<p> Shirley McWhirl, you have sunk to impossibly new lows.</p>
+<p className="character">Rusty</p>
+<p> Who’s Shirley?</p>
+<p className="character">Dorothy</p>
+<p> AND SHIRLEY Her.</p>
+<p className="character">Rusty</p>
+<p> I’m sorry, I’m supposed to speak with Dorothy Dot. How am I to know who is whom?</p>
+<p className="character">Shirley</p>
+<p> You know I’m Dorothy, because I can tell you that I was born in Kansas and moved here as a young newspaper woman.</p>
+<p className="character">Dorothy</p>
+<p> You know I’m Dorothy because I can tell you that none of that is correct. And if I had a cell phone I could prove it.</p>
+<p className="character">Shirley</p>
+<p> A what?</p>
+<p className="character">Dorothy</p>
+<p> Nothing.</p>
+<p className="character">Shirley</p>
+<p> I’m the newspaper’s best reporter and have been working on this story for months. You’re the last piece of the puzzle.</p>
+<p className="character">Dorothy</p>
+<p> My goodness, you will stop at nothing to get this scoop. Well let me ask you one thing, Mr.- Would Shirley know my signature song?</p>
+<p className="stage">She starts singing.</p>
+<p className="character">Dorothy (CONT’D)</p>
+<p>  I’m Dorothy Dot, I’ve got a regular spot, above our paper’s fold, ‘cuz my writing is gold, and I’ve got a Pulitzer and awards galore, I’ve covered the streets, I’ve covered the war, and I work in the top floor and can see the skyline, if you don’t believe me just check my byline!</p>
+<p className="character">Rusty</p>
+<p> I can’t take this anymore! The mayor pays off the police chief so he’ll arrest all his political rivals.</p>
+<p className="stage">Dorothy and Shirley stop.</p>
+<p className="character">Rusty (CONT’D) </p>
+<p> That’s it, and until he’s exposed, he’ll keep doing it. Too many good men have been put away just so he won’t have to face a threat to his power.</p>
+<p className="stage">Dorothy and Shirley look at each other and go to run out, but bump into each other. They fight, with Dorothy knocking Shirley down. She runs out, leaving Dorothy behind, on the floor. Blackout. Lights up. Dorothy is holding an envelope and is speaking with Fred in hushed tones when Shirley bursts in, out of breath and looking beat up. Shirley laughs maniacally.</p>
+<p className="character">Shirley</p>
+<p> Got you- I’ve got you.</p>
+<p className="character">Fred</p>
+<p> What’s she talking about?</p>
+<p className="character">Shirley</p>
+<p> Check her story, Fred. Oh wait, you can’t, it’s off to the presses already. Tonight, everyone will find out what you’ve done, Miss Dot, and you’ll be out of this business forever.</p>
+<p className="stage">Shirley walks over to Dorothy and rips the envelope from her hands.</p>
+<p className="character">Shirley (CONT’D)</p>
+<p>  This, ladies and gentlemen, is proof that she doesn’t deserve the front page. She doesn’t even deserve the funnies.</p>
+<p className="stage">Shirley takes the story out of the envelope and starts to read.</p>
+<p className="character">Shirley (CONT’D) </p>
+<p> Breaking news. The mayor is running a speakeasy with girls, wiseguys, and gin...Wait a minute. This is-</p>
+<p className="character">Dorothy</p>
+<p> The old story? The one that was wrong because you set me up?</p>
+<p className="character">Shirley</p>
+<p> You switched them. You already switched them?</p>
+<p className="character">Dorothy</p>
+<p> That’s right, Lady McWhirl. My story, with the delicious scoop that is the take down of Mayor McCloud is running in tonight’s paper, and it’s gonna win me another award and loads of readers to boot.</p>
+<p className="character">Fred</p>
+<p> I’m sorry, Shirley, but if this is how you’re going to act around here, I just don’t see you staying at this paper.</p>
+<p className="character">Shirley</p>
+<p> What? You can’t be saying-</p>
+<p className="character">Fred</p>
+<p> You’re fired. Maybe next time you’ll learn from your fellow reporters instead of trying to ruin them.</p>
+<p className="stage">Fred leaves.</p>
+<p className="character">Shirley</p>
+<p> This isn’t the last you’ve heard of me, Dorothy Dot.</p>
+<p className="character">Dorothy</p>
+<p> But it is the last I’ll read of you. Now if you’ll excuse me, dear, I’ve got another story to write.</p>
+<p className="stage">Dorothy leaves in triumph, Shirley slumps down.  Blackout. End of play. </p>
+
+</section>
+</div>
+
+
+
+  </div>
+);
+
+export default DeadlinePage;
