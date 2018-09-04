0f9fd0cfcf6c1231e5d16f791428d09f3d762235
Kelsey Oates <kelsey.oates@gmail.com>
2018-09-04 09:40:14 -0400

    Add files via upload

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -23,7 +23,7 @@ export default class ContactForm extends React.Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => alert("/thanks"))
+      .then(() => action("/thanks"))
       .catch(error => alert(error));
 
     e.preventDefault();
